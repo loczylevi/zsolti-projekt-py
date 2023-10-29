59c584f66143363d1a7db26a820ebeab90c22b5b
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2023-10-29 20:33:46 +0100

    Create holding_key.py

---
+++ holding_key.py
@@ -0,0 +1,22 @@
+import pyautogui
+import time
+
+
+time.sleep(5)    # várjál 5 másodpercet
+#pyautogui.hotkey('ctrl', 'a')
+
+with pyautogui.hold('ctrl'):          # tarcsd le nyomva a ctrl gombot!
+    pyautogui.press("tab")
+    time.sleep(1)
+    pyautogui.press("tab")
+    time.sleep(1)
+    pyautogui.press("tab")
+    time.sleep(1)
+    pyautogui.press("tab")
+    time.sleep(1)
+    pyautogui.press("tab")
+    time.sleep(1)
+    pyautogui.press("tab")
+    time.sleep(1)
+    pyautogui.press("tab")
+    time.sleep(1)
