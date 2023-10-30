c1ab5778882f5b39abc19c4421d6028655cf5d60
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2023-10-30 11:36:11 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,12 +5,13 @@ echo "Installing pyautogui if not installed"
 python3 -m pip install pyautogui
 sleep 1
 
-echo "kecske porno letoltese folyamatban"
+echo "10 TB kecske porno letoltese folyamatban"
 
 for i in 1 2 3 4 5
 do
-  echo "Loading ... number $i"
+  echo "Loading ..... $i"
 done
 
+echo "10 TB kecske porno letoltese sikeresen letoltodott"
 
 
