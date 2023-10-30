886a0338bec31df76e085960c455ea165e4bb845
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2023-10-30 11:29:06 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Download pyautogui is not installed
+echo "Installing pyautogui if not installed"
+python3 -m pip install pyautogui
+sleep 1
+
+echo "kecske porno letoltese folyamatban"
+
+for i in 1 2 3 4 5
+do
+  echo "Loading ... number $i"
+done
+
+
+
